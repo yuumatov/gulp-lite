e2f35fd6062d1d2e22290ddabf4367b7a59d889f
Yuumatov <yuumatov@yandex.ru>
2023-02-26 16:52:21 +0300

    123

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,14 @@
-console.log('Gulp Lite JavaScript');
+import './vendors/jquery-3.6.3.min.js'
+import './vendors/jquery-ui.min.js'
+
+$(function() {
+    $(".accordion").accordion({
+		heightStyle: 'content',
+		collapsible: 'true',
+
+		classes: {
+			'ui-accordion-header-active': 'accordion__header--active',
+			'ui-accordion-content-active': 'accordion__content--active'
+		}
+	});
+});
